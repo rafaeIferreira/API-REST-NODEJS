ad78c44167075e9abed29410bfaabdc0ba01dd99
Rafael Ferreira <raffaferr@gmail.com>
2024-05-29 00:10:10 -0300

    setting up tests

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,5 @@
-import fastify from 'fastify'
+import { app } from './app'
 import { env } from '../env'
-import { transactionRoutes } from './routes/transactions'
-import cookie from '@fastify/cookie'
-
-const app = fastify()
-
-app.register(cookie)
-
-app.register(transactionRoutes, {
-  prefix: 'transactions',
-})
 
 app
   .listen({
